1820c4743c3d8214636ee5eeafd41ba030851a2f
evan-gaus-neu <gaus.e@northeastern.edu>
2021-11-02 12:41:14 -0400

    Adding switch to controller

--- as4/src/controller/ImageProControllerImpl.java
+++ as4/src/controller/ImageProControllerImpl.java
@@ -1,5 +1,8 @@
 package controller;
 
+import java.io.IOException;
+import java.util.Scanner;
+
 import model.ImageProModel;
 import view.ImageProView;
 
@@ -30,6 +33,85 @@ public class ImageProControllerImpl implements ImageProController {
 
   @Override
   public void run() throws IllegalStateException {
+
+    // Important data
+    Scanner scan = new Scanner(in);
+    boolean quit = false;
+    // Other stored things
+
+    // Do a welcome message?
+
+    // Loop while not quit
+
+    while (!quit) {
+
+      // Print message
+      try {
+        view.renderMessage("Type instruction: \n");
+      }
+      catch (IOException e) {
+        throw new IllegalStateException(e.getMessage());
+      }
+
+      // Get the next input (QQ maybe check if there is one first)
+      String userInput = scan.next();
+
+      // Switch for userInput
+      switch (userInput) {
+        case "load": // load image
+          // Do stuff here
+          break;
+        case "save": // save image
+          // Do stuff here
+          break;
+        case "red-component": // save image
+          // Do stuff here
+          break;
+        case "green-component": // save image
+          // Do stuff here
+          break;
+        case "blue-component": // save image
+          // Do stuff here
+          break;
+          // value, intensity, luma
+        case "value-component": // save image
+          // Do stuff here
+          break;
+        case "intensity-component": // save image
+          // Do stuff here
+          break;
+        case "luma-component": // save image
+          // Do stuff here
+          break;
+        case "horizontal-flip": // save image
+          // Do stuff here
+          break;
+        case "vertical-flip": // save image
+          // Do stuff here
+          break;
+        case "brighten": // save image
+          // Do stuff here
+          break;
+        case "q":
+        case "quit":
+          quit = true;
+          break;
+        default: // Unrecognized command
+          try {
+            view.renderMessage("Unrecognized Command: " + userInput + "\n");
+          }
+          catch (IOException e) {
+            throw new IllegalStateException(e.getMessage());
+          }
+
+
+      }
+
+
+
+
+    }
+
 
   }
 
