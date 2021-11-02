5d4da5998eb493fd4816afa466abf1de48ef71d1
VinnietheGenius <vinniejonesjeyakumar@gmail.com>
2021-11-02 16:26:07 -0400

    Modify

--- as4/src/model/ImageProModelImpl.java
+++ as4/src/model/ImageProModelImpl.java
@@ -34,69 +34,66 @@ public class ImageProModelImpl implements ImageProModel {
 
 
     ImageProModelImpl newModel = this;
-    for(int i = 0; i < newModel.image.length; i++) {
-      for(int j = 0; j < newModel.image[i].length; j++) {
-        ColorPixel pixel = new ColorPixel(newModel.image[i][j].getR(),
-                newModel.image[i][j].getG(), newModel.image[i][j].getB());
+    for(int i = 0; i < newImage.length; i++) {
+      for(int j = 0; j < newImage[i].length; j++) {
+        ColorPixel pixel = new ColorPixel(oldImage[i][j].getR(),
+                oldImage[i][j].getG(), oldImage[i][j].getB());
         if (input.equals("Red")) {
-          newModel.image[i][j] = new ColorPixel(pixel.getR(), pixel.getR(), pixel.getR());
+          newImage[i][j] = new ColorPixel(pixel.getR(), pixel.getR(), pixel.getR());
         }
         else if (input.equals("Green")) {
-          newModel.image[i][j] = new ColorPixel(pixel.getG(), pixel.getG(), pixel.getG());
+          newImage[i][j] = new ColorPixel(pixel.getG(), pixel.getG(), pixel.getG());
         }
         else if (input.equals("Blue")) {
-          newModel.image[i][j] = new ColorPixel(pixel.getB(), pixel.getB(), pixel.getB());
+          newImage[i][j] = new ColorPixel(pixel.getB(), pixel.getB(), pixel.getB());
         }
         else if (input.equals("Value")) {
           int max = Math.max(pixel.getB(), Math.max(pixel.getR(), pixel.getG()));
-          newModel.image[i][j] = new ColorPixel(max, max, max);
+          newImage[i][j] = new ColorPixel(max, max, max);
         }
         else if (input.equals("Intensity")) {
           int average = (int) Math.round((pixel.getR() + pixel.getG() + pixel.getB())/3.0);
-          newModel.image[i][j] = new ColorPixel(average, average, average);
+          newImage[i][j] = new ColorPixel(average, average, average);
         }
         else {
           int luma = (int) Math.round(0.2126 * pixel.getR() + 0.7152 * pixel.getG()
                   + 0.0722 * pixel.getB());
-          newModel.image[i][j] = new ColorPixel(luma, luma, luma);
+          newImage[i][j] = new ColorPixel(luma, luma, luma);
         }
       }
     }
-
     // Instead of returning
     images.put(dest, newImage);
-
-    return newModel;
   }
 
   @Override
   public void redComponent(String name, String dest) {
-    return colorComponent("Red");
+    colorComponent("Red", name, dest);
   }
 
   @Override
   public void greenComponent(String name, String dest) {
-    return colorComponent("Green");
+    colorComponent("Green", name, dest);
   }
 
   @Override
   public void blueComponent(String name, String dest) {
-    return colorComponent("Blue");
+    colorComponent("Blue", name, dest);
   }
 
   @Override
   public void valueComponent(String name, String dest) {
-    return colorComponent("Value");
+    colorComponent("Value", name, dest);
   }
 
   @Override
   public void intensityComponent(String name, String dest) {
-    return colorComponent("Intensity");
+    colorComponent("Intensity", name, dest);
   }
 
   @Override
   public void lumaComponent(String name, String dest) {
-    return colorComponent("Luma");
+    colorComponent("Luma", name, dest);
   }
 
   @Override
